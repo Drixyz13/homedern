ae7b21f658a6e6ad90478799dba0ae83eb4e7fa9
Drixyz13 <mattmariller@gmail.com>
2025-07-28 12:43:53 +0200

    finalizing product view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+      @pod   = Product.find_by(name: "Homedern Pod")
+     @temps  = Product.find_by(name: "Homedern Temps")
+     @fridge = Product.find_by(name: "Homedern Fridge")
   end
 end
