899f0a06c325b7766a3308ea02bb3442cd7939b4
Drixyz13 <mattmariller@gmail.com>
2025-07-30 17:41:20 +0200

    before action and private method added for simplicity in various controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,21 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :set_orders, only: [:index]
+  before_action :set_order, only: [:show]
   def index
-    @orders = current_user.orders.includes(:order_items, :products).order(created_at: :desc)
+    @orders
   end
 
   def show
-    @order = current_user.orders.find(params[:id])
+    @order
   end
 
+  def set_orders
+    @orders = current_user.orders.includes(:order_items, :products).order(created_at: :desc)
+  end
+
+  def set_order
+    @order = current_user.orders.find(params[:id])
+  end
  
 end
--- app/controllers/stripe_checkout_controller.rb
+++ app/controllers/stripe_checkout_controller.rb
@@ -1,9 +1,8 @@
 class StripeCheckoutController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_product, only: [:create, :success]
 
   def create
-    product = Product.find(params[:product_id])
-
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [
@@ -11,45 +10,48 @@ class StripeCheckoutController < ApplicationController
           price_data: {
             currency: 'eur',
             product_data: {
-              name: product.name,
-              description: product.description
+              name: @product.name,
+              description: @product.description
             },
-            unit_amount: product.price_cents
+            unit_amount: @product.price_cents
           },
           quantity: 1
         }
       ],
       mode: 'payment',
-      success_url: success_stripe_checkout_url + "?product_id=#{product.id}&session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: product_url(product)
+      success_url: success_stripe_checkout_url + "?product_id=#{@product.id}&session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: product_url(@product)
     )
 
     redirect_to session.url, allow_other_host: true
   end
 
-    def success
+  def success
     session_id = params[:session_id]
-    product = Product.find(params[:product_id])
     session = Stripe::Checkout::Session.retrieve(session_id)
 
     order = current_user.orders.create!(
-      total_cents: product.price_cents,
+      total_cents: @product.price_cents,
       stripe_payment_id: session_id,
       status: "paid"
     )
 
-    
     order.order_items.create!(
-      product: product,
+      product: @product,
       quantity: 1,
-      price_at_order: product.price_cents
+      price_at_order: @product.price_cents
     )
 
     OrderMailer.confirmation_email(order).deliver_now
 
-    redirect_to product_path(product), notice: "Merci pour votre achat !"
+    redirect_to product_path(@product), notice: "Merci pour votre achat !"
   rescue Stripe::InvalidRequestError
     redirect_to root_path, alert: "Erreur de confirmation du paiement."
   end
 
+  private
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
 end
